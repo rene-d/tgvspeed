be89773a96c271446066e310032888c95cf4f15b
rene-d <rene-d@users.noreply.github.com>
2024-09-13 14:03:36 +0200

    icône d'app

--- setup.py
+++ setup.py
@@ -9,8 +9,12 @@ from setuptools import setup
 
 APP = ["tgvspeed.py"]
 DATA_FILES = ["480px-Robot_icon_broken.svg.png", "gps.png", "train.png", "map.png", "destination.png"]
-OPTIONS = {}
-
+OPTIONS = {
+    "iconfile": "icon.icns",
+    "plist": {
+        "CFBundleShortVersionString": "0.1.0",
+    },
+}
 setup(
     app=APP,
     data_files=DATA_FILES,
